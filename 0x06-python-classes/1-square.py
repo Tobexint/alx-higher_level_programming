1ebaba065890d199b6ffdeac4dda387156ae5cd3
Tobexint <tobexint@gmail.com>
2022-07-19 16:59:51 -0700

    __size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Define class Square with private attribute size
+"""
+
+
+class Square:
+    """Represent a square."""
+
+    def __init__(self, size):
+        """
+        Initializes square """
+
+        self.__size = size
