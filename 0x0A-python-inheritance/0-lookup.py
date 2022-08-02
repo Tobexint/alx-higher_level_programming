2fbe0f12822e82ed22d58eaf24ed0341ae766205
Tobexint <tobexint@gmail.com>
2022-08-01 19:17:00 -0700

    dir

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""
+Finding a list of available attributes and methods of an object.
+"""
+
 def lookup(obj):
     """ Function that returns the list of available attributes
         and methods of an object
