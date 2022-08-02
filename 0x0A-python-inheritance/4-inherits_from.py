81230946b1e1a62138a089861cdb277bc63414b4
Tobexint <tobexint@gmail.com>
2022-08-01 20:00:02 -0700

    instance

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Finds if the object is an instance of a class that inherited
+(directly or indirectly) from the specified class.
+"""
+
+
+def inherits_from(obj, a_class):
+    """ Function that returns True/False if obj is an instance of a_class
+    Args:
+        obj: object
+        a_class: class type
+    Returns:
+        True if obj is an instance of a_class
+        False, otherwise
+    """
+    return isinstance(obj, a_class) and type(obj) != a_class
