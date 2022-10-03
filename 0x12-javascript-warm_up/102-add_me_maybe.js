acd187e99e3f4218a0d14304fe474f0b2f387648
tobexint <tobexint@gmail.com>
2022-10-03 15:29:41 -0700

    script

---
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+//  function that increments and calls a function.
+//  The function must be visible from outside
+exports.addMeMaybe = function (number, theFunction) {
+  theFunction(++number);
+};
