ca724a5fecc4038ff44a4379792efa9898e02314
tobexint <tobexint@gmail.com>
2022-10-04 09:34:12 -0700

    constructor

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,11 +1,12 @@
 #!/usr/bin/node
 // The constructor must take 2 arguments w and h
 // Initialize the instance attribute width with the value of w
-// Initialize the instance attribute height with the value of h
 class Rectangle {
   constructor (w, h) {
-    this.width = w;
-    this.height = h;
+    if (((w = parseInt(w)) > 0) && ((h = parseInt(h)) > 0)) {
+      this.width = w;
+      this.height = h;
+    }
   }
 }
 module.exports = Rectangle;
