7f0fa280a8fbfdc30d27114dc1537c4e82926a5d
tobexint <tobexint@gmail.com>
2022-10-07 23:16:46 -0700

    update

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """lists all states with a name starting with N
 (upper N) from the database hbtn_0e_0_usa
-Your script should take 3 arguments: mysql username, mysql password and database name (no argument validation needed)
+Your script should take 3 arguments: mysql username,
+mysql password and database name (no argument validation needed)
 Your script should connect to a MySQL server running on localhost at port 3306
 Results must be sorted in ascending order by states.id
 """
