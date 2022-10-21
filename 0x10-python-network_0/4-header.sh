be40d42c26b709827f62716f1c3d54027968f589
tobexint <tobexint@gmail.com>
2022-10-20 19:14:05 -0700

    header

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # script that takes in a URL as an argument, sends a GET request to the URL
-# A header variable X-School-User-Id must be sent with the value 98
-curl -sH "X-School-User-Id: 98" "$1"
+curl -sL -H 'X-School-User-Id: 98' "$1"
