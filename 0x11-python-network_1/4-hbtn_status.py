c0dbf8d8c9afb1ec16aa2f8440fbb051645f8a73
tobexint <tobexint@gmail.com>
2022-10-21 20:20:20 -0700

    requests

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Python script that fetches https://alx-intranet.hbtn.io/status
+using the requests module
+"""
+if __name__ == "__main__":
+    import sys
+    import requests
+
+    r = requests.get('https://alx-intranet.hbtn.io/status')
+    print('Body response:')
+    print('\t- type: {}'.format(type(r.text)))
+    print('\t- content: {}'.format(r.text))
